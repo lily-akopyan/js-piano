ff289be19510cfc29cf993c7e5a9e654750dab12
lily-akopyan <lily.akopyan@gmail.com>
2023-06-22 11:19:30 +0200

    add functionality on click

--- script.js
+++ script.js
@@ -12,5 +12,19 @@ function removeActive(e) {
     key.classList.remove("active");
 }
 
+function clickPlaySound(e){
+    const audio = new Audio();
+    audio.src = `sounds/${e.target.id}.wav`;
+    audio.play();
+   // document.querySelector(`audio[src="${e.target.id}"`).play();
+}
+
+/*document.querySelectorAll('.key').forEach(key => key.addEventListener('click', e => {
+    //document.querySelector(`audio[src="${e.target.id}"`).play();
+}));
+*/
+document.querySelectorAll('.key').forEach(key => key.addEventListener('click', clickPlaySound, {
+    capture: true
+}));
 window.addEventListener('keydown', playSound);
 window.addEventListener('keyup', removeActive);
